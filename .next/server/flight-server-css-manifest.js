6c48353d15f2f89a9132d9563de3fe3c920b7f7e
Mshanga Makonde <41159327+mshanga@users.noreply.github.com>
2023-09-12 08:44:53 +0300

    .gitignore

---
+++ .next/server/flight-server-css-manifest.js
@@ -0,0 +1,10 @@
+self.__RSC_CSS_MANIFEST={
+  "__entry_css_mods__": {
+    "/Users/manu/Documents/GitHub/makonde/app/page": [
+      "/Users/manu/Documents/GitHub/makonde/app/globals.css"
+    ]
+  },
+  "/Users/manu/Documents/GitHub/makonde/app/layout.tsx": [
+    "/Users/manu/Documents/GitHub/makonde/app/globals.css"
+  ]
+}
